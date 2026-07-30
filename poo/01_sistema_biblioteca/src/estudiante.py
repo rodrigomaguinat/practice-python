550014229c9efd8ad54bf4190cd244d95351b9a1
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2026-07-30 17:57:12 -0500

    feat(poo-01): create estudiante.py with Estudiante class

---
+++ poo/01_sistema_biblioteca/src/estudiante.py
@@ -0,0 +1,9 @@
+class Estudiante:
+    def __init__(self, código, nombres, apellidos, correo, carrera, ciclo, teléfono):
+        self.código = código
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.correo = correo
+        self.carrera = carrera
+        self.ciclo = ciclo
+        self.teléfono = teléfono
