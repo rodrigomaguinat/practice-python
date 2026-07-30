5238b38db7099b176de1534ac9d7a39fcb001050
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2026-07-30 17:06:50 -0500

    feat(poo-01): create libro.py with Libro class

---
+++ poo/01_sistema_biblioteca/libro.py
@@ -0,0 +1,10 @@
+class Libro:
+    def __init__(self, código, ISBN, título, autor, editorial, año, categoría, cantidad):
+        self.código = código
+        self.ISBN = ISBN
+        self.título = título
+        self.autor = autor
+        self.editorial = editorial
+        self.año = año
+        self.categoría = categoría
+        self.cantidad = cantidad
